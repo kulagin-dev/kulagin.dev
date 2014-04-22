01abc2b6ff9cd264885b5de75135c9de160d7176
vanburg <edjbox@gmail.com>
2014-04-22 08:34:25 +0400

    first css version

--- Gemfile
+++ Gemfile
@@ -1,14 +1,9 @@
-# If you have OpenSSL installed, we recommend updating
-# the following line to use "https"
-source 'http://rubygems.org'
+source 'https://rubygems.org'
 
-gem "middleman", "~>3.3.2"
+gem 'middleman', github: 'middleman', branch: 'v3-stable'
+gem 'middleman-livereload', github: 'middleman/middleman-livereload'
+gem 'middleman-deploy'
+gem 'middleman-slim'
 
-# Live-reloading plugin
-gem "middleman-livereload", "~> 3.1.0"
-
-# For faster file watcher updates on Windows:
-gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
-
-# Windows does not come with time zone data
-gem "tzinfo-data", platforms: [:mswin, :mingw]
+gem 'bootstrap-sass', '~>3.1.1'
+gem 'oily_png'
--- config.rb
+++ config.rb
@@ -1,72 +1,58 @@
-###
-# Compass
-###
+# Some info
+# https://github.com/beef/beef-middleman/blob/master/config.rb
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+# SLIM
 
-###
-# Page options, layouts, aliases and proxies
-###
+set :slim, { pretty: true, format: :html5 }
 
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
+helpers do
+  def some_helper
+    "Helping"
+  end
+end
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+# COMPASS
 
-###
-# Helpers
-###
+compass_config do |config|
+  config.output_style = :compact
+end
 
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
+# SPROCKETS
 
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+set :debug_assets, true
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+configure :development do
+  activate :livereload do |lr|
+    lr[:no_swf] = true
+  end
+end
 
-set :css_dir, 'stylesheets'
+# LIVERELOAD
 
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
+activate :livereload do |lr|
+  lr.no_swf = true
+end
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
+end
 
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+# Middleman in dev mode allows duplicated values
+after_configuration do
+  config[:file_watcher_ignore].push(/^.idea/).uniq!
+end
+
+activate :deploy do |deploy|
+  deploy.method = :sftp
+  deploy.host = 'srv'
+  deploy.user = 'vbru'
+  deploy.port = 22
+  deploy.path = 'public'
+  deploy.build_before = true
 end
